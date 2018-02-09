c998e11bd7a168c84ba96ef7b6c22ffa7caf0426
vkandy <vkandy@gmail.com>
2018-02-09 13:50:50 +0100

    Routes to create a shipment if it doesn't exist. Added tests.

--- server/routes/telemetry.js
+++ server/routes/telemetry.js
@@ -11,18 +11,54 @@ let router = express.Router();
  * Using GET to make it easier to send sensor data
  */
 router.get('/send', function(req, res) {
-    // account=123&shipmentId=123&temp=22&humidity=38&airpressure=1011
     let owner       = req.query.owner;
-    let secret       = req.query.secret;
+    let secret      = req.query.secret;
     let shipmentId  = req.query.shipmentId;
     let name        = req.query.name;
     let temp        = req.query.temp;
     let humidity    = req.query.humidity;
     let airpressure = req.query.airpressure;
 
-    console.log(req.query);
+    debug(req.query);
 
-    let options = {
+    if(!shipmentId || !name) {
+        return res.status(400).json({err: "shipmentId and name are required"});
+    }
+
+    rp({
+        uri: "https://network.ambrosus.com/assets/find/shipmentId:" + shipmentId,
+        json: true
+    })
+    .then(function(assets) {
+        if(assets.length === 0) {
+            debug("Shipment not found. Creating shipment as a new asset.");
+            return createAsset(owner, secret, shipmentId);
+        } else {
+            debug("Found a shipment");
+            return assets[0];
+        }
+    })
+    .then(function(asset) {
+        debug("Adding events to asset:", asset);
+        return createEvent(owner, secret, asset.id, temp, humidity, airpressure);
+    })
+    .then(function(parsedBody) {
+        return res.status(200).json(parsedBody);
+    })
+    .catch(function(err) {
+        return res.status(500).json({err: err});
+    });
+});
+
+/**
+ * Returns a promise which creates a new asset.
+ * @param owner
+ * @param secret
+ * @param shipmentId
+ * @returns {*}
+ */
+function createAsset(owner, secret, shipmentId) {
+    return rp({
         method: 'POST',
         uri: "https://network.ambrosus.com/assets/",
         body: {
@@ -40,15 +76,39 @@ router.get('/send', function(req, res) {
             }
         },
         json: true
-    };
-
-    rp(options)
-    .then(function(parsedBody) {
-        return res.status(200).json(parsedBody);
-    })
-    .catch(function(err) {
-        return res.status(500).json({err: err});
     });
-});
+}
+
+/**
+ * Returns a promise to POST http request.
+ * @param owner
+ * @param secret
+ * @param assetId
+ * @param temp
+ * @param humidity
+ * @param airpressure
+ * @returns {*}
+ */
+function createEvent(owner, secret, assetId, temp, humidity, airpressure) {
+    return rp({
+        method: 'POST',
+        uri: "https://network.ambrosus.com/assets/" + assetId + "/events",
+        body: {
+            "content": {
+                "secret": secret,
+                "data": {
+                    "type": "Telemetry",
+                    "subject": assetId,
+                    "creator": owner,
+                    "created_at": Date.now(),
+                    "temp": temp,
+                    "humidity": humidity,
+                    "airpressure": airpressure
+                }
+            }
+        },
+        json: true
+    });
+}
 
 module.exports = router;
---
+++ test/Shipment.spec.js
@@ -0,0 +1,31 @@
+/**
+ * Shipment scenarios.
+ */
+const Shipment  = artifacts.require("./Shipment.sol");
+const Inventory = artifacts.require("./Inventory.sol");
+
+contract('Shipment API', (accounts) => {
+
+    const [owner] = accounts;
+
+    before(async () => {
+        this.inventory  = await Inventory.deployed();
+        this.shipmentId = "Shipment-123";
+
+        await this.inventory.addShipment(owner, this.shipmentId, "Tylenol", 1, 5, 40, 50, 1100, 1150);
+        let shipments = await this.inventory.shipments.call();
+        console.log(shipments);
+    });
+
+    describe("Storage and retrieval", () => {
+
+        it("should store sensor readings", async () => {
+            await this.inventory.addTelemetry(owner, this.shipmentId, "000", 4, 45, 100);
+        });
+
+        it("should trigger a violation event", async () => {
+            let result = await this.inventory.addTelemetry(owner, this.shipmentId, "000", 25, 45, 1100);
+        });
+    });
+
+});
